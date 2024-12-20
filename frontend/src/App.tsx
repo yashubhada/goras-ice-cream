e425e5c5430cbd20b7cfc94fd1d116fcc3a776a8
Yash <cdyash99@gmail.com>
2024-12-20 10:27:13 +0530

    start project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App: React.FC = () => {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline text-red-500">
+                Hello world!
+            </h1>
+        </>
+    )
+}
+
+export default App
